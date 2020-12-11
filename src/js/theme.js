880029231bfd29b451a085c1faa4c397ae969c02
Maksym Zyza <mmziza88@gmail.com>
2020-12-11 20:12:36 +0200

    видалив зайве

--- src/js/theme.js
+++ src/js/theme.js
@@ -6,14 +6,8 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
-localStorage.setItem('Theme', JSON.stringify(Theme))
-const value = localStorage.getItem('Theme')
-const parsedValue = JSON.parse(value)
-console.log(parsedValue);
-
-// 1) При нажатии(событие change) на чекбокс #theme -switch-toggle - добавлять на элемент body класс light-theme или dark-theme.
 function handleClick() {
-    if (bodyRef.className === '') {
+  if (bodyRef.className === '') {
     bodyRef.classList.add(Theme.DARK);
   } else {
     bodyRef.classList.toggle(Theme.LIGHT);
@@ -27,22 +21,7 @@ function handleClick() {
 
 changeRef.addEventListener("change", handleClick);
 
-// 2) Выбранная тема сохраняется между перезагрузками страницы. Для хранения темы используй localStorage.
-// Если при загрузке страницы тема тёмная, не забудь поставить свойство checked у чекбокса #theme-switch-toggle в true, чтобы ползунок сдвинулся в правильное положение.
 
-function restoreTheme() {
-  const savedTheme = localStorage.getItem('theme');
-
-  if (savedTheme) {
-    bodyRef.classList.add(savedTheme);
-  }
-
-  if (savedTheme === Theme.DARK) {
-    changeRef.setAttribute('checked', true);
-  }
-}
-
-restoreTheme();
 
 
 
